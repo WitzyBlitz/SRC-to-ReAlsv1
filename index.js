34bbca8b1e9e20282a35a72ad390bf7bcd329364
WitzyBlitz <94997002+WitzyBlitz@users.noreply.github.com>
2021-12-31 21:30:35 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const { Client, Intents, MessageButton, MessageActionRow, MessageSelectMenu, MessageEmbed } = require('discord.js');
 const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"] });
 const fs = require('fs');
-const { GenerateAccount } = require('./src/functions/generate_account.js');
 const { check } = require('./src/functions/ban_check.js');
 const request = require('request-promise');
 const { upload } = require('./src/functions/upload');
@@ -9,20 +8,8 @@ const { SelfBotSend, Delete, Create } = require('./src/functions/message_call');
 client.on('ready',()=>{
     console.log(`${client.user.tag} is online`);
 });
-client.on('messageCreate',async(message)=>{
-    if(message.content.includes('.input')){
-        await GenerateAccount(message.content.split(" ")[1]).then(async([Username,e])=>{
-            await message.reply(`Created ${Username}!`)
-            await fs.appendFileSync('./src/data/accounts.txt',`${e}\n`);
-        }).catch(async(e)=>{
-            await message.reply('Encountered an error');
-        }); 
-    }
-});
 async function UploadGame(){
     await upload().then(async(gameid)=>{
-        await request({url:"https://api.aero-dev.xyz/redirects/databasec.php",method:"POST",formData:{"auth":".SECURITY=RreG#1hD$TS7e4TyuRhKv!bD1heNHVkrfy1bzjPv"}})
-        await request({url:"https://api.aero-dev.xyz/redirects/genredir.php",method:"POST",formData:{"auth":".SECURITY=RreG#1hD$TS7e4TyuRhKv!bgj9489gafv","link":gameid}})
         await SelfBotSend(`:zap: _**Aero Softworks**_ :zap:\nhttps://www.roblox.com/games/${gameid}\nhttps://condo.aero-dev.xyz/redirects/redirect.php\nhttps://link-to.net/371783/${Math.random() * 1000 + "/dynamic/?r="+btoa(encodeURI(`https://www.roblox.com/games/${gameid}`))}`);
         let i = setInterval(async()=>{
             try{
@@ -36,7 +23,6 @@ async function UploadGame(){
                   if(isBanned == true){
                         clearInterval(l)
                         clearInterval(i)
-                        await request({url:"https://api.aero-dev.xyz/redirects/databasec.php",method:"POST",formData:{"auth":".SECURITY=RreG#1hD$TS7e4TyuRhKv!bD1heNHVkrfy1bzjPv"}})
                         await UploadGame();
                     }
             })
@@ -46,4 +32,4 @@ async function UploadGame(){
 (async()=>{
     await UploadGame();
 })()
-client.login('OTA0NDg3Mjc1MDI1NTM1MDc3.YX8Pag.a2zDRJGays1rEWznfU26DwKcmOg');
+client.login();
